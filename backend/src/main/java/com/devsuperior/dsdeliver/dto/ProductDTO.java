e2fe4bad292353ebc363b92ecc6e3e9fb4e8b9a4
rqguzman <rafael.guzman@outlook.com>
2021-01-05 09:33:51 -0300

    Add endpoint GET /products

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/dto/ProductDTO.java
@@ -0,0 +1,43 @@
+package com.devsuperior.dsdeliver.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dsdeliver.entities.Product;
+
+import lombok.Data;
+
+@Data
+public class ProductDTO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	
+	private String name;
+	
+	private Double price;
+	
+	private String description;
+	
+	private String imageUri;
+
+	public ProductDTO() {
+	}
+
+	public ProductDTO(Long id, String name, Double price, String description, String imageUri) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.description = description;
+		this.imageUri = imageUri;
+	}
+
+	public ProductDTO(Product entity) {
+		id = entity.getId();
+		name = entity.getName();
+		price = entity.getPrice();
+		description = entity.getDescription();
+		imageUri = entity.getImageUri();
+	}
+	
+}
