786fb44bc365ea742409462b2881998cf5d72368
rqguzman <rafael.guzman@outlook.com>
2021-01-08 17:57:59 -0300

    Add search order location

--- front-web/src/Order/OrderLocation.tsx
+++ front-web/src/Order/OrderLocation.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import AsyncSelect from 'react-select/async';
 import { fetchLocalMapBox } from '../api';
+import { OrderLocationData } from './types';
 
 const initialPosition = {
   lat: -22.914381,
@@ -17,7 +18,11 @@ type Place = {
   };
 }
 
-function OrderLocation () {
+type Props = {
+  onChangeLocation: (location: OrderLocationData) => void;
+}
+
+function OrderLocation ({ onChangeLocation: onChangeLocation }: Props) {
 
   const [address, setAddress] = useState<Place>({
     position: initialPosition
@@ -42,11 +47,11 @@ function OrderLocation () {
   
   const handleChangeSelect = (place: Place) => {
     setAddress(place);
-    // onChangeLocation({
-    //   latitude: place.position.lat,
-    //   longitude: place.position.lng,
-    //   address: place.label!
-    // });
+    onChangeLocation({
+      latitude: place.position.lat,
+      longitude: place.position.lng,
+      address: place.label!
+    });
   };
   
   return(
--- front-web/src/Order/index.tsx
+++ front-web/src/Order/index.tsx
@@ -2,12 +2,13 @@ import { useEffect, useState } from 'react';
 import './styles.css';
 import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
-import { Product } from './types';
+import { OrderLocationData, Product } from './types';
 import { fetchProducts } from '../api';
 import OrderLocation from './OrderLocation';
 
 function Orders () {
   const[products, setProducts] = useState<Product[]>([]);
+  const[orderLocation, setOrderLocation] = useState<OrderLocationData>();
   
   useEffect(() => {
     fetchProducts()
@@ -20,7 +21,9 @@ function Orders () {
      <div className="orders-container">
        <StepsHeader />
        <ProductsList products={products}/>
-       <OrderLocation />
+       <OrderLocation 
+          onChangeLocation={location => setOrderLocation(location)}
+       />
      </div>
     </>
   )
